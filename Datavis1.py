6e71cbd5d80b6f88e979f995d92791ed71af929e
Chelsea Lembert <102269641+chelsea-lembert@users.noreply.github.com>
2024-02-18 15:06:33 -0500

    Add files via upload

---
+++ Datavis1.py
@@ -0,0 +1,39 @@
+import csv
+import matplotlib.pyplot as mp
+
+categories = []
+years = []
+values = []
+
+with open('histcpi-CLEAN.csv', newline='') as file:
+    reader = csv.reader(file)
+
+    for row in reader:
+        categories.append(row[0])
+
+        year_values = []
+        for value in row[1:]:
+            try:
+                year_values.append(float(value))
+
+            except ValueError:
+                year_values.append(None)
+        years.append([int(year) for year in range(1974, 2023)])
+        values.append(year_values)
+
+mp.figure(figsize=(20, 12))
+
+for i in range(len(categories)):
+    filtered_values = [value for value in values[i] if value is not None]
+    filtered_years = [str(year) for j, year in enumerate(range(1974, 2023)) if values[i][j] is not None]
+    mp.bar(filtered_years, filtered_values, label=categories[i])    
+
+
+mp.xlabel('Yaer')
+mp.ylabel('CPI Values')
+mp.title('CPI Values Over The Years')
+mp.legend()
+
+
+mp.show()
+
